d79bd1f31e5a98fb1c9639b831817529afaeae1d
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-11-25 10:22:39 +0800

    add changes'

--- app/Http/Controllers/DeathController.php
+++ app/Http/Controllers/DeathController.php
@@ -20,7 +20,7 @@ class DeathController extends Controller
         
         $validator = validator($request->all(), [
             "name" => "required|unique:deaths,name",
-            "start_date" => "required|date|after:today",
+            "start_date" => "required|date",
             "end_date" => "required|date|after:start_date",
             "profession" => "required",
             "user_id" => "required|exists:users,id"
